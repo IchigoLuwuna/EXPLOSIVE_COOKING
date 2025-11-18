d3fa5604ab807d076513e40a9c1d030b8fa60ffc
Pype-Luna <luna.pype@student.howest.be>
2025-11-18 16:43:31 +0100

    Added menus

---
+++ menus.s
@@ -0,0 +1,43 @@
+state_menu_start:
+state_menu_start_init:
+	; Flush OAM
+	ldy #$00
+	:
+		lda #$00
+		sta $0200, y
+		iny
+	cpy #$FF
+	bmi :-
+
+state_menu_start_loop:
+@forever:
+	jsr func_get_input	; get controller input and store in joypad ($00)
+	lda joypad
+	and #PAD_START
+	cmp #PAD_START
+	bne :+
+		and reg_c
+		cmp #PAD_START
+		beq :+ ; skip if start is held
+		jmp state_game
+	:
+
+jmp @forever
+
+state_menu_pause:
+state_menu_pause_loop:
+@forever:
+	lda joypad
+	sta reg_c ; store state of joypad on previous frame in reg_c -> allows for non-repeating actions on held input
+	jsr func_get_input	; get controller input and store in joypad ($00)
+	lda joypad
+	and #PAD_START
+	cmp #PAD_START
+	bne :+
+		and reg_c
+		cmp #PAD_START
+		beq :+ ; skip if start is held
+		jmp state_game_loop
+	:
+
+jmp @forever
