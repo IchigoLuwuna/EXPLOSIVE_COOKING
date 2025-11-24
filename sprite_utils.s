309c2922433d40efa338d0e998993539819f0fe2
Pype-Luna <luna.pype@student.howest.be>
2025-11-24 09:40:45 +0100

    Added sprite_utils

---
+++ sprite_utils.s
@@ -0,0 +1,6 @@
+; Move an arbitrary 16x16 sprite by an arbitrary x and y value
+; A->Low byte of sprite address
+; X->Handled as signed int, movement on X axis
+; Y->Handled as signed int, movement on Y axis
+func_move_16x16:
+rts
