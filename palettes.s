27e84ed0742ec924cdef1adfac0d74095d877111
Luna <luna.pype06@gmail.com>
2025-12-07 20:22:40 +0100

    Changed palettes

--- palettes.s
+++ palettes.s
@@ -8,7 +8,7 @@ palettes:
 .byte $0f, $16, $1a, $11
 
 ; Sprite Palette
+.byte $0f, $16, $1a, $11 ; RGB
 .byte $0f, $16, $1a, $11
 .byte $0f, $16, $1a, $11
-.byte $0f, $16, $1a, $11
-.byte $0f, $16, $1a, $11
+.byte $0f, $30, $30, $30 ; black and white
