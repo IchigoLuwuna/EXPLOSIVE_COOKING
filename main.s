175d076273acd61346beafa712e3616d19f22af9
Pype-Luna <luna.pype@student.howest.be>
2025-12-10 20:28:25 +0100

    Changed header

--- main.s
+++ main.s
@@ -1,9 +1,9 @@
 .segment "HEADER"
-	; .byte "NES", $1A      ; iNES header identifier
-	.byte $4E, $45, $53, $1A
-	.byte 2               ; 2x 16KB PRG code
-	.byte 1               ; 1x  8KB CHR data
+	.byte "NES", $1A      ; iNES header identifier
+	.byte $02             ; 2x 16KB PRG code
+	.byte $01             ; 1x  8KB CHR data
 	.byte $01, $00        ; mapper 0, vertical mirroring
+	.byte $00, $01        ; PAL
 
 .segment "ZEROPAGE_DATA"
 	reg_b = $00 ; 1bt: extra B register
