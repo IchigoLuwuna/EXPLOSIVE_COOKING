e21f70fc53c4d58bfb3372162fe74240cbc28727
Pype-Luna <luna.pype06@gmail.com>
2025-12-15 01:24:51 +0100

    Changed palette

--- palettes.s
+++ palettes.s
@@ -3,14 +3,14 @@ palettes:
 
 ; Background Palette
 
-.byte $0b, $2D, $3D, $39 ; black and white
-.byte $0b, $30, $2D, $3C ; blueish
-.byte $0b, $17, $27, $26 ; brownish 
-.byte $0b, $16, $26, $29 ; greenish
+.byte $0f, $2D, $3D, $39 ; black and white
+.byte $0f, $30, $2D, $3C ; blueish
+.byte $0f, $17, $27, $26 ; brownish
+.byte $0f, $16, $26, $29 ; greenish
 
 ; Sprite Palette
-.byte $0b, $0f, $19, $36   ;
-.byte $0b, $16, $1a, $11
-.byte $0b, $16, $1a, $11
-.byte $0b, $30, $30, $30 ; black and white
+.byte $0f, $0f, $19, $36   ;
+.byte $0f, $16, $1a, $11
+.byte $0f, $16, $1a, $11
+.byte $0f, $30, $30, $30 ; black and white
 
