d37107c107898d84c23626e7057077fafb07c48f
FinnVerbeeck <239849759+CatInSand@users.noreply.github.com>
2026-01-19 18:16:07 +0100

    Optimized code and rewrote comments

--- input.s
+++ input.s
@@ -1,46 +1,53 @@
-cport1 = $4016  ; hardware address of controller port 1
-cport2 = $4017  ; hardware address of controller port 1
+CPORT1_ADDR = $4016  ; hardware address of controller port 1
+CPORT2_ADDR = $4017  ; hardware address of controller port 1
 
+; ---------------------------------------------
+; get input from controller and zapper
+; return: joypad state -> joypad (zpg)
+; return: zapper state -> zapper (zpg)
+; ---------------------------------------------
 func_get_input:
-    ; not pushing register because function is called at start of loop
-
-    ; function start
     lda joypad  ; save previous joypad state
     sta joypad_previous
 
+    ; ----------------------
     ; get joypad input
+    ; ----------------------
+
     ; poll controller 1 input
-    lda #$01
-    sta cport1  ; write 1 to controller port 1 to start polling input data
-    lda #$00
-    sta cport1  ; write 0 to controller port 1 to end polling input data/lip
+    lda #$01    ; write 1 to controller port 1 to start polling input data
+    sta CPORT1_ADDR
+    lda #$00    ; write 0 to controller port 1 to end polling input data/lip
+    sta CPORT1_ADDR
 
     ; read controller 1 data
 	ldx #$00 ; for(int i{}; i < 8; ++i)
 	@loop:
-		lda cport1
-		lsr ; loads bit 0 into carry
-		ror joypad ; shifts carry left into joypad
+		lda CPORT1_ADDR
+		lsr         ; loads next bit into carry
+		ror joypad  ; shifts carry left into joypad
 		inx
 		cpx #$08
 		bmi @loop
 	; bit order is now [Right, Left, Down, Up, Start, Select, B, A]
 	; 				   [    7,    6,    5,  4,     3,      2, 1, 0]
 
+    ; ----------------------
     ; get zapper input
-    ; poll controller 1 input
-    lda #$01
-    sta cport2  ; write 1 to controller port 1 to start polling input data
-    lda #$00
-    sta cport2  ; write 0 to controller port 1 to end polling input data/lip
+    ; ----------------------
 
-    lda cport2
-	sta zapper ; shifts carry left into joypad
+    ; poll zapper input
+    lda #$01    ; write 1 to controller port 2 to start polling input data
+    sta CPORT2_ADDR
+    lda #$00    ; write 0 to controller port 2 to end polling input data/lip
+    sta CPORT2_ADDR
+
+    lda CPORT2_ADDR
+	sta zapper
 	; bit order is [ /, /, /, Trigger, Light, /, /, Serial data]
 	; 			   [ 7, 6, 5,       4,     3, 2, 1,           0]
     ; Trigger: 0 is released or pulled, 1 is half pulled
     ; Light: 0 is detected, 1 not detected
 
-    ; function end
-
-    rts ; return from subroutine
+func_get_input_end:
+    rts
--- random.s
+++ random.s
@@ -1,71 +1,81 @@
+; ---------------------------------------------
+; set seed for random number generation equal to clock (#$00 is invalid so use #$7f instead)
+; seed stored in lfsr
+; ---------------------------------------------
 func_seed_random:
-    ; set seed for random number generation equal to clock
-    ; a seed of 0 does not work
     lda clock
-    cmp #$00    ; if seed is 0, set to 7f
+    cmp #$00    ; if seed is #$00, set to #$7f
     bne :+
 		lda #$7f
     :
     sta lfsr    ; store seed in lfsr
 
+func_seed_random_end:
     rts
 
+
+; ---------------------------------------------
+; get a new random number (using 8bit linear feedback shift register)
+; return: random number -> register A
+; ---------------------------------------------
 func_random_to_acc:
-    ; push registers
-    php     ; push processor
-    txa     ; push x
-    pha
     lda reg_b   ; push register b
     pha
 
-    ; start function
+    ; ----------------------------------
+    ; calculate next random number
+    ; ----------------------------------
 
-    ; store bit 7 in register b
-    lda #%10000000
-    and lfsr    ; isolate bit 7
-    jsr func_acc_to_bool
+    ; store bit 7 in register B
+    lda #%10000000          ; isolate bit 7
+    and lfsr
+    jsr func_acc_to_bool    ; convert to bool
     sta reg_b
-    ; xor bit 5 with register b and store in register b
-    lda #%00100000
-    and lfsr    ; isolate bit 5
-    jsr func_acc_to_bool
-    eor reg_b
-    sta reg_b
-    ; xor bit 4 with register b and store in register b
-    lda #%00010000
-    and lfsr    ; isolate bit 4
-    jsr func_acc_to_bool
-    eor reg_b
-    sta reg_b
-    ; xor bit 3 with register b and store in a
-    lda #%00001000
-    and lfsr    ; isolate bit 3
-    jsr func_acc_to_bool
-    eor reg_b
 
-    ; shift lfsr and or with x
+    ; xor bit 5 with bit 7
+    lda #%00100000          ; isolate bit 5
+    and lfsr
+    jsr func_acc_to_bool    ; convert to bool
+    eor reg_b               ; xor with bit 7
+    sta reg_b
+
+    ; xor bit 4 with result of previous xor
+    lda #%00010000          ; isolate bit 4
+    and lfsr
+    jsr func_acc_to_bool    ; convert to bool
+    eor reg_b               ; xor with result of previous xor
+    sta reg_b
+
+    ;; xor bit 3 with result of previous xor
+    lda #%00001000          ; isolate bit 3
+    and lfsr
+    jsr func_acc_to_bool    ; convert to bool
+    eor reg_b               ; xor with result of previous xor
+
+    ; shift lfsr and shift in new bit
     asl lfsr
-    ora lfsr
+    ora lfsr    ; add new bit
     sta lfsr
 
-    ; end function
-
-    ; pull registers
-    pla  ; pullh register b
+func_random_to_acc_end:
+    pla         ; pull register b
     sta reg_b
-    pla     ; pull x
-    tax
-	plp     ; pull processor
 
-    lda lfsr
+    lda lfsr    ; return random number in register A
 
-    rts     ; return from subroutine
+    rts
 
+
+; ---------------------------------------------
+; turns accumulator into boolean (helper function for func_random_to_acc)
+; param: shifted boolean -> register A
+; return: #$00 or #$01 -> register A
+; ---------------------------------------------
 func_acc_to_bool:
-    ldx #%00000000
-    cmp #$00    ; turn accumulator into boolean
-    beq :+
-        ldx #%00000001
+    cmp #$00
+    beq :+  ; if register A != #$00, return #$01
+        lda #%00000001
     :
-    txa
+
+func_acc_to_bool_end:
     rts
